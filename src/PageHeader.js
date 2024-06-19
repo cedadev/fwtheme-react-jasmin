5e66e4480fd84d3c013755b69eb439a8302ddd8e
Nicola Farmer <nicola.farmer@stfc.ac.uk>
2024-06-19 16:37:44 +0100

    Update file extension of react-bootstrap imports

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row.js';
+import Col from 'react-bootstrap/Col.js';
 
 
 const PageHeader = props => (
